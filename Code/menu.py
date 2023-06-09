8cd30fdf1e694e0b5ff776bf0abb7b9b4268a497
KyleBerberi <135790369+KyleBerberi@users.noreply.github.com>
2023-06-09 18:16:08 -0400

    Update menu.py

--- Code/menu.py
+++ Code/menu.py
@@ -4,17 +4,23 @@ from random import randint
 from pgzero.keyboard import keyboard
 import random
 from subprocess import call
-
+import time
 
 global music
 WIDTH = 800
 HEIGHT = 554
 player = Actor('mc.png')
 bg = Actor('bg.png')
-endscreen1 = Actor ('endscreen1.png')
+heart1 = Actor('heart.png')
+heart2 = Actor('heart.png')
+heart3 = Actor('heart.png')
+endscreen1 = Actor('endscreen1.png')
 endscreen2 = Actor('endscreen2.png')
 endscreen3 = Actor('endscreen3.png')
 
+heart1.pos =(10,10)
+heart2.pos =(10,10)
+heart3.pos =(10,10)
 player.pos = (400, 470)
 bullets = []
 enemy_bullets = []
@@ -25,17 +31,18 @@ kong = Actor("enemy")
 kong.pos = (WIDTH // 2, 50)
 kong.velocity = 2
 
-
 tracks = ['menu', 'menu2', 'menu3']
-track = randint(0,2)
+track = randint(0, 2)
 music.set_volume(0.3)
 music.play(tracks[track])
-
 
 start = Rect((270, 170), (250, 100))
 setting = Rect((270, 400), (250, 100))
 exit = Rect((270, 280), (250, 100))
 
+end_screen_timer = 0
+SHOW_END_SCREEN_DURATION = 5  # in seconds
+SHOW_END_SCREEN2_DURATION = 3  # in seconds
 
 def draw():
     global gamestate
@@ -44,25 +51,25 @@ def draw():
     screen.clear()
     if gamestate == 0:
         bg.draw()
-    elif gamestate ==1:
+    elif gamestate == 1:
         screen.blit('jungle', (0, 0))
         player.draw()
+    if lives = 3:
+        heart1.draw()
+        heart2.draw()
+        heart3.draw()
+    if lives = 2:
         kong.draw()
         for bullet in bullets:
             bullet.draw()
         for enemy_bullet in enemy_bullets:
             enemy_bullet.draw()
-            health_bar_width = health * 2
-            screen.draw.filled_rect(Rect((50, 50), (health_bar_width, 20)), "red")
-            """RED = 200, 0, 0
-            BLUE = 0, 0, 200
-            GREEN = 0, 200, 0
-            screen.draw.rect(setting, RED)
-            screen.draw.rect(start, BLUE)
-            screen.draw.rect(exit, GREEN)"""
+        health_bar_width = health * 2
+        screen.draw.filled_rect(Rect((50, 50), (health_bar_width, 20)), "red")
     elif gamestate == 10:
         endscreen1.draw()
-
+    elif gamestate == 2:
+        endscreen2.draw()
 
 def on_mouse_down(pos):
     global gamestate
@@ -75,7 +82,7 @@ def on_mouse_down(pos):
         print('Menu')
 
 def update():
-    global gamestate
+    global gamestate, end_screen_timer
     if gamestate == 1:
         global health, lives
 
@@ -118,6 +125,8 @@ def update():
                     lives -= 1
                     if lives == 0:
                         gamestate = 10
+                        end_screen_timer = time.time()
+
         kong.x += kong.velocity
 
         if kong.left < 0 or kong.right > WIDTH:
@@ -127,7 +136,15 @@ def update():
 
         if health <= 0:
             gamestate = 2
-            print("Enemy defeated!")
+            end_screen_timer = time.time()
+
+    elif gamestate == 10:
+        if time.time() - end_screen_timer >= SHOW_END_SCREEN_DURATION:
+            gamestate = 0
+
+    elif gamestate == 2:
+        if time.time() - end_screen_timer >= SHOW_END_SCREEN2_DURATION:
+            gamestate = 0
 
 def on_key_down(key):
     global keys
@@ -145,7 +162,5 @@ def fire_enemy_bullet():
     enemy_bullet.vy = 3
     enemy_bullet.pos = (kong.x + 20, kong.y + 30)
     enemy_bullets.append(enemy_bullet)
-
-
 
 pgzrun.go()
